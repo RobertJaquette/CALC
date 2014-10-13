8bb22f119be50877e86310087e8852ea81b4b9ac
Robester06 <jaquette@udel.edu>
2014-10-13 05:07:18 -0400

    ps4 calc

---
+++ CalcEngine/src/MainPackage/Calc.java
@@ -0,0 +1,18 @@
+
+package MainPackage;
+
+public class Calc {
+	
+	public static double futureValue (double InvestmentAmount, int years, double Annualrate){
+
+		return (InvestmentAmount * Math.pow((1+(Annualrate/100)), years));
+		
+	}
+	public void setText(String string) {	
+	}
+		
+	}
+
+	
+
+		 
